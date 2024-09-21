7d9b84a5a95eab52608d7808ced06f049b53d545
Daniel Yang <me@danielyang.dev>
2024-09-20 23:10:48 -0700

    counts each letter and prints in descending

--- main.py
+++ main.py
@@ -2,7 +2,29 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    chars_dict = get_char_count(text)
+    list_of_chars = convert_dict_to_array(chars_dict)
+    list_of_chars.sort(reverse=True, key=sort_on)
+    print(list_of_chars)
+
+def sort_on(dict):
+    return dict["count"]
+
+def convert_dict_to_array(dict):
+    array = []
+    for entry in dict:
+        if entry.isalpha():
+            array.append({"name": entry, "count": dict[entry]})
+    return array
+
+def get_char_count(text):
+    dictionary_of_characters = {}
+    for c in text.lower():
+        if c in dictionary_of_characters:
+            dictionary_of_characters[c] += 1
+        else:
+            dictionary_of_characters[c] = 1 
+    return dictionary_of_characters
 
 def get_num_words(text):
     words = text.split()
